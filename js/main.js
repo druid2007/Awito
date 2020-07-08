4141f1a9effcc7b7bc5f791de91d8dfec1ac95d0
druid2007 <fedotov2007@yandex.ru>
2020-07-08 18:46:40 +0500

    dz1

--- js/main.js
+++ js/main.js
@@ -5,8 +5,11 @@ const modalAdd = document.querySelector('.modal');
 const addAd = document.querySelector('.add__ad');
 const modalBtnSubmin = document.querySelector('.modal__btn-submit');
 const modalSubmit = document.querySelector('.modal__submit');
+const modalItem = document.querySelector('.modal__item');
+// const card = document.querySelector('.card');
+const catalog = document.querySelector('.catalog');
 
-
+// Работа с модалным окном Добавить объявление
 document.body.addEventListener('keyup', (event) => {
     if (event.keyCode == 27) {
         modalAdd.classList.add('hide');
@@ -22,16 +25,27 @@ addAd.addEventListener('click', () => {
 
 modalAdd.addEventListener('click', (event) => {
     const target = event.target;
-
     if (target.classList.contains('modal__close') ||
         target === modalAdd) {
         modalAdd.classList.add('hide');
         modalSubmit.reset();
     }
-
-
-    console.log(event);
 })
 
+// Работа с модалным окном Карточки
 
-console.log(modalAdd);
+catalog.addEventListener('click', (event) => {
+    const target = event.target;
+    // console.log(target);
+    const card = target.closest('.card');
+    modalItem.classList.remove('hide');
+
+})
+
+modalItem.addEventListener('click', (event) => {
+    const target = event.target;
+    if (target.classList.contains('modal__close') ||
+        target === modalItem) {
+        modalItem.classList.add('hide');
+    }
+})
